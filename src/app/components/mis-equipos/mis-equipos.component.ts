34cee78166e841c3bce8ba898e4dfe0f6be89bcf
dohimenezg <djimenezg@unicauca.edu.co>
2024-09-08 22:59:43 -0500

    navegation added

--- src/app/components/mis-equipos/mis-equipos.component.ts
+++ src/app/components/mis-equipos/mis-equipos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-mis-equipos',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterLink,],
   templateUrl: './mis-equipos.component.html',
   styleUrl: './mis-equipos.component.css'
 })
